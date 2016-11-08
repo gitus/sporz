5c2143080c094d449b55c5e915dc8e434280e8fa
Simon Lieb <pips@e5150.fr>
2016-11-08 22:09:46 +0100

    Framework from hell!

---
+++ app/Models/Player.php
@@ -0,0 +1,16 @@
+<?php
+class Player extends Pragma\ORM\Model
+{
+	private $name;
+	private $keyId;
+	private $genome;
+	private $role;
+	private $paralysed;
+	private $mutated;
+
+	public function __construct()
+	{
+		return parent::__construct();
+	}
+
+}
