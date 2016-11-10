8fa68d569d0e208b800c6e0b41e8308c99a36000
Simon Lieb <pips@e5150.fr>
2016-11-10 22:35:09 +0100

    Add basic layout using bootstrap
    https://getbootstrap.com

---
+++ app/Views/layout.tpl.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Sporz</title>
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	</head>
+	<body>
+	<?php $this->yields(); ?>
+	</body>
+</html>
