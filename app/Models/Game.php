b3e73a7e6f4a1265ae8144718d7d46d3c731f5be
git <git@shroomrider.com>
2016-11-10 01:25:13 +0100

    some random ideas to get things started

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -2,12 +2,16 @@
 
 namespace App\Models;
 
+define("DAY",0);
+define("NIGHT",1);
+
 class Game extends \Pragma\ORM\Model
 {
 	private $players;
 	private $id;
 	private $started;
 	private $phase;
+	private $leader;
 
 	public function __construct()
 	{
@@ -30,9 +34,24 @@ class Game extends \Pragma\ORM\Model
 		return $keyId;
 	}
 
+	public function getPlayerByRole($role){
+		$tmp=array();
+		foreach($this->players as $player){
+			if($role == $player->role){
+				array_push($tmp,$player);
+			}
+		}
+		if(count($tmp)>1){
+			return $tmp;
+		}
+		if(count($tmp)==1){
+			return $tmp[0];
+		}
+		return null;
+	}
 	public function getPlayerByName($name){
 		foreach($this->players as $player){
-			if($name == $player->getName()){
+			if($name == $player->name){
 				return $player;
 			}
 		}
@@ -47,6 +66,9 @@ class Game extends \Pragma\ORM\Model
 	}
 
 	public function initGame(){
+		if(count($this->players)<7){
+			return false;
+		}
 		shuffle($this->players);
 
 		$bad_guy=array_shift($this->players);
@@ -54,15 +76,48 @@ class Game extends \Pragma\ORM\Model
 		$bad_guy->setGenome(-1);
 
 		$first_doc=array_shift($this->players);
-		$fist_doc->setRole("medecin");
+		$fist_doc->role="medic";
 
 		$second_doc=array_shift($this->players);
-		$second_doc->setRole("medecin");
+		$second_doc->role="medic";
 
 		$index_of_modified_genomes=array_rand($this->players, 2);
 		$this->players[$index_of_modified_genomes[0]]->setGenome(-1);
 		$this->players[$index_of_modified_genomes[1]]->setGenome(1);
 
 		array_push($this->players, $bad_guy, $first_doc, $second_doc);
+		
+		$some_guy=array_shift($this->players);
+		$some_guy->role="psy";
+		array_push($this->players,$some_guy);
+		$some_guy=array_shift($this->players);
+		$some_guy->role="geneticist";
+		array_push($this->players,$some_guy);
+		$some_guy=array_shift($this->players);
+		$some_guy->role="it";
+		array_push($this->players,$some_guy);
+		$some_guy=array_shift($this->players);
+		$some_guy->role="hacker";
+		array_push($this->players,$some_guy);
+		return true;
+	}
+
+	public function electLeader($name){
+		$wanabe=getPlayerByName($name);
+		if($wanabe && $wanabe->alive){
+			$this->leader=$name;
+			return true;
+		}
+		return false;
+	}
+
+	public static function getNonStartedGame(){
+		$joinable=Game::all();
+		foreach($joinable as $k=>$val){
+			if($val->started){
+				unset($joinable[$k]);
+			}
+		}
+		return $joinable;
 	}
 }
--- app/Models/Player.php
+++ app/Models/Player.php
@@ -10,10 +10,39 @@ class Player extends \Pragma\ORM\Model
 	private $role;
 	private $paralysed;
 	private $mutated;
+	private $alive;
 
-	public function __construct()
+	public function __construct($name)
 	{
 		return parent::__construct('player');
+		$this->name=$name;
+	}
+
+	public function setGenome($genome){
+		if($genome>0){
+			$genome=1;
+		}elseif($genome<0){
+			$genome=-1;
+		}else{
+			$genome=0;
+		}
+		$this->genome=$genome;
+	}
+		
+	public function mutate(){
+		if($this->genome<1){
+			$this->mutated=1;
+			return true;
+		}
+		return false;
+	}
+
+	public function cure(){
+		if($this->genome>-1 || !$this->mutated){
+			$this->mutated=0;
+			return true;
+		}
+		return false;
 	}
 
 }
