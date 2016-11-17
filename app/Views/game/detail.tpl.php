fddf7ad1f7c08e2a49768a7aa7a6759159824692
Simon Lieb <pips@e5150.fr>
2016-11-17 22:20:31 +0100

    Display player count on game detail

--- app/Views/game/detail.tpl.php
+++ app/Views/game/detail.tpl.php
@@ -1,6 +1,7 @@
 <h2><?= $this->get('game')->name; ?></h2>
 <a href="<?= $this->get('edit-link'); ?>">Edit</a>
 <div>
+    Players: <?= count($this->get('game')->players); ?><br>
     Created at: <?= $this->get('game')->created; ?><br>
     Last modified: <?= $this->get('game')->updated; ?>
 </div>
