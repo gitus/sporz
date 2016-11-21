90b41b1feecf8df84708f51f6d2d43cd42832217
Simon Lieb <pips@e5150.fr>
2016-11-21 22:17:58 +0100

    Retab

--- app/Views/index.tpl.php
+++ app/Views/index.tpl.php
@@ -2,25 +2,25 @@
 <div class="table-responsive">
 	<table class="table table-striped">
 		 <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Name</th>
-                  <th>Player count</th>
-                  <th>Joined</th>
-                </tr>
-              </thead>
-              <tbody>
-    <?php
-    foreach ($this->get('joinableGames') as $game) {
-        ?>
-        <tr>
-            <a href="<?= $this->get('router')->url_for('game-detail', ['gameid' => $game->id]); ?>">
-                <td><?= $game->id; ?></td>
-                <td><?= $game->name; ?></td>
-                <td><?= $game->name; ?></td>
-                <td><?= $game->name; ?></td>
-            </a>
-        </tr>
-        <?php
-    }
-    ?>
+				<tr>
+				  <th>#</th>
+				  <th>Name</th>
+				  <th>Player count</th>
+				  <th>Joined</th>
+				</tr>
+			  </thead>
+			  <tbody>
+	<?php
+	foreach ($this->get('joinableGames') as $game) {
+		?>
+		<tr>
+			<a href="<?= $this->get('router')->url_for('game-detail', ['gameid' => $game->id]); ?>">
+				<td><?= $game->id; ?></td>
+				<td><?= $game->name; ?></td>
+				<td><?= $game->name; ?></td>
+				<td><?= $game->name; ?></td>
+			</a>
+		</tr>
+		<?php
+	}
+	?>
